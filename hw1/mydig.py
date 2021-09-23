9ce87aa1199a20483bcd702083282ce6fbc1d33d
evenyoung <yiyun@igola.com>
2021-09-23 13:31:33 -0400

    [HW1]fix bugs, ignore 'CNAME'

--- hw1/mydig.py
+++ hw1/mydig.py
@@ -15,64 +15,39 @@ import dns.rdatatype
 import random
 
 
-def dns_resolver(query_domain, query_type: dns.rdatatype):
+def dns_resolver(cur_domain, cur_type: dns.rdatatype):
     # this list is copied from www.iana.org/domains/root/servers
     root_server_list = ['198.41.0.4', '199.9.14.201', '192.33.4.12', '199.7.91.13', '192.203.230.10', '192.5.5.241',
                         '192.112.36.4', '198.97.190.53', '192.36.148.17', '192.58.128.30', '193.0.14.129',
                         '199.7.83.42', '202.12.27.33']
     random.shuffle(root_server_list)  # shuffle root_server list, so that it can be visited randomly
 
-    # 1. query root server
-    root_resp = query_from_root(root_server_list, query_type, query_domain)
+    # 1. issue request to root server
+    root_resp = issue_request(root_server_list, cur_type, cur_domain)
     if root_resp is None:
         print(f'Request for all root servers failed')
         return
 
-    # 2. query TLD
-    tld_resp = query_from_additional(root_resp, query_type, query_domain)
+    # 2. issue request to TLD
+    tld_ip_list = to_ipv4_list(root_resp.additional)
+    tld_resp = issue_request(tld_ip_list, cur_type, cur_domain)
     if tld_resp is None:
         print(f'Request for all TLD servers failed')
         return
 
-    # If queried type is NS, then we can stop here
-    if query_type == dns.rdatatype.NS:
-        return tld_resp.authority
-
-    # 3. query Name Server
+    # 3. issue request to Name Server
     prev_resp = tld_resp
-    while True:
-        ns_resp = query_from_additional(prev_resp, query_type, query_domain)
-        prev_additional = prev_resp.additional
-        if ns_resp is None:
-            prev_cname = get_cname(prev_additional)
-            if prev_cname is not None:      # Only CNAME records returned in PREVIOUS answer, query by CNAME
-                return dns_resolver(prev_cname, query_type)
-            print(f'Request for all Name Servers failed')
-            return
-        # check if result is in the answer
-        for ans in ns_resp.answer:
-            if ans.rdtype == query_type:
-                return ns_resp.answer
-        cur_cname = get_cname(ns_resp.answer)
-        if cur_cname is not None:           # Only CNAME records returned in CURRENT answer, query by CNAME
-            return dns_resolver(cur_cname, query_type)
-        prev_resp = ns_resp                 # No matching records in current answer, continue query by 'additional'
+    while not check_ans(cur_type, prev_resp):
+        if len(prev_resp.answer) != 0:      # answer is returned but CNAME only
+            return prev_resp.answer
+
+        ns_ipv4_list = to_ipv4_list(prev_resp.additional)   # answer is empty
+        prev_resp = issue_request(ns_ipv4_list, cur_type, cur_domain)
+    return prev_resp.answer
 
 
-def query_from_root(root_server_list, query_type, query_domain):
-    for ip in root_server_list:
-        try:
-            resp = single_query(query_domain, query_type, ip)
-            if resp.rcode() == dns.rcode.NOERROR:
-                return resp
-        except Exception as e:
-            print(f'query server {ip} type {dns.rdatatype.to_text(query_type)} failed: {e}')
-    print(f'Request for all Root Servers failed')
-
-
-def query_from_additional(prev_resp, query_type, query_domain):
-    for rr_set in ipv4_records(prev_resp.additional):
-        ip = get_ip(rr_set)
+def issue_request(ipv4_list, query_type, query_domain):
+    for ip in ipv4_list:
         try:
             resp = single_query(query_domain, query_type, ip)
             if resp.rcode() == dns.rcode.NOERROR:
@@ -82,29 +57,25 @@ def query_from_additional(prev_resp, query_type, query_domain):
     print(f'Request for all Servers failed')
 
 
-def single_query(query_domain, query_type: dns.rdatatype, dst_ip, timeout=10):
+def single_query(query_domain, query_type: dns.rdatatype, dst_ip, timeout=20):
     req = dns.message.make_query(query_domain, query_type)
     return dns.query.udp(req, dst_ip, timeout)
 
 
-def get_ip(rr_set: dns.rrset.RRset):
+def get_rdata(rr_set: dns.rrset.RRset):
     for item in rr_set.items:
         return item.to_text()
 
 
-def get_rdtype(rr_set_list: dns.rrset.RRset):
-    for rr_set in rr_set_list:
-        return rr_set.rdtype
+def to_ipv4_list(rr_set_list):
+    return [get_rdata(x) for x in rr_set_list if x.rdtype == dns.rdatatype.A]
 
 
-def get_cname(rr_set_list: dns.rrset.RRset):
-    for rr_set in rr_set_list:
-        if rr_set.rdtype == dns.rdatatype.CNAME:
-            return get_ip(rr_set)
-
-
-def ipv4_records(rr_set_list):
-    return [x for x in rr_set_list if x.rdtype == dns.rdatatype.A]
+def check_ans(query_type: dns.rdatatype, resp):
+    for ans in resp.answer:
+        if ans.rdtype == query_type:
+            return True
+    return False
 
 
 def main():
@@ -114,10 +85,7 @@ def main():
     rr_set_list = dns_resolver(query_domain, dns.rdatatype.from_text(rdtype))
     end_time = time.time()
 
-    output = []
-    output.append(f'QUESTION SECTION:\n{query_domain}			IN	{rdtype}\n')
-    output.append("ANSWER SECTION: ")
-
+    output = [f'QUESTION SECTION:\n{query_domain}			IN	{rdtype}\n', "ANSWER SECTION: "]
     for rr_set in rr_set_list:
         cur_name = rr_set.name
         cur_class = dns.rdataclass.to_text(rr_set.rdclass)
