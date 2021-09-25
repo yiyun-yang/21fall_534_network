3d718788d0cc7dc8864040b6b873aa1a16b660eb
evenyoung <yiyun@igola.com>
2021-09-24 21:05:18 -0400

    [HW1]mydig.py: make root_servers a global variable

--- hw1/mydig.py
+++ hw1/mydig.py
@@ -15,11 +15,13 @@ import dns.rdatatype
 import random
 
 
+# this list is copied from www.iana.org/domains/root/servers
+root_server_list = ['198.41.0.4', '199.9.14.201', '192.33.4.12', '199.7.91.13', '192.203.230.10', '192.5.5.241',
+                    '192.112.36.4', '198.97.190.53', '192.36.148.17', '192.58.128.30', '193.0.14.129',
+                    '199.7.83.42', '202.12.27.33']
+
+
 def dns_resolver(cur_domain, cur_type: dns.rdatatype, result_list):
-    # this list is copied from www.iana.org/domains/root/servers
-    root_server_list = ['198.41.0.4', '199.9.14.201', '192.33.4.12', '199.7.91.13', '192.203.230.10', '192.5.5.241',
-                        '192.112.36.4', '198.97.190.53', '192.36.148.17', '192.58.128.30', '193.0.14.129',
-                        '199.7.83.42', '202.12.27.33']
     random.shuffle(root_server_list)  # shuffle root_server list, so that it can be visited randomly
 
     # 1. issue request to root server
