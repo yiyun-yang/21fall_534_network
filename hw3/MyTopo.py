2a11dfbaadd2f5388109b10c4c8bfdba12c49486
evenyoung <yiyun@igola.com>
2021-12-18 03:56:44 -0500

    [hw3]fix A1

--- hw3/MyTopo.py
+++ hw3/MyTopo.py
@@ -3,16 +3,10 @@
 """
 @author: Yiyun Yang
 """
-"""Custom topology example
-Adding the 'topos' dict with a key/value pair to generate our newly defined
-topology enables one to pass in '--topo=mytopo' from the command line.
-"""
-
-import pdb
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.node import Node
-from mininet.log import setLogLevel, info
+from mininet.log import setLogLevel, info, debug
 from mininet.cli import CLI
 
 
@@ -32,60 +26,56 @@ class LinuxRouter(Node):
 class NetworkTopo(Topo):
     def build(self, **_opts):
         # Add routers
-        r1 = self.addHost('r1', cls=LinuxRouter, ip='10.0.0.1/24')
+        r1 = self.addHost('r1', cls=LinuxRouter)
         r2 = self.addHost('r2', cls=LinuxRouter)
         r3 = self.addHost('r3', cls=LinuxRouter)
-        r4 = self.addHost('r4', cls=LinuxRouter, ip='10.0.1.1/24')
+        r4 = self.addHost('r4', cls=LinuxRouter)
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
 
-        # Adding hosts specifying the default route
-        # subnet: 10.0.0.0/24
-        h1 = self.addHost(name='h1', ip='10.0.0.2/24', defaultRoute='via 10.0.0.1')
+        # Adding host-router connection
         self.addLink(h1, r1, intfName1='h1-eth0', intfName2='r1-eth0')
-        # subnet: 10.0.1.0/24
-        h2 = self.addHost(name='h2', ip='10.0.1.2/24', defaultRoute='via 10.0.1.1')
         self.addLink(h2, r4, inftname1='h2-eth0', inftname2='r4-eth0')
 
         # Add router-router connection
-        self.addLink(r1, r2, intfName1='r1-eth1', intfName2='r2-eth1')
-        self.addLink(r3, r1, intfName1='r3-eth1', intfName2='r1-eth2')
-        self.addLink(r2, r4, intfName1='r2-eth2', intfName2='r4-eth1')
-        self.addLink(r4, r3, intfName1='r4-eth2', intfName2='r3-eth2')
+        self.addLink(r1, r2, intfName1='r1-eth1', intfName2='r2-eth0')
+        self.addLink(r1, r3, intfName1='r1-eth2', intfName2='r3-eth0')
+        self.addLink(r2, r4, intfName1='r2-eth1', intfName2='r4-eth1')
+        self.addLink(r3, r4, intfName1='r3-eth1', intfName2='r4-eth2')
 
 
 def run():
     topo = NetworkTopo()
-    net = Mininet(topo=topo)
-
-    r1 = net['r1']
-    r2 = net['r2']
-    r3 = net['r3']
-    r4 = net['r4']
-
-    # subnet: 192.168.10.128/25
-    r1.cmd('ifconfig r1-eth1 192.168.10.176/25')
-    r2.cmd('ifconfig r2-eth1 192.168.10.177/25')
-    # subnet: 192.168.11.128/25
-    r3.cmd('ifconfig r3-eth1 192.168.11.177/25')
-    r1.cmd('ifconfig r1-eth2 192.168.11.176/25')
-    # subnet: 192.168.12.128/25
-    r2.cmd('ifconfig r2-eth2 192.168.12.177/25')
-    r4.cmd('ifconfig r4-eth1 192.168.12.176/25')
-    # subnet: 192.168.13.128/25
-    r4.cmd('ifconfig r4-eth2 192.168.13.176/25')
-    r3.cmd('ifconfig r3-eth2 192.168.13.177/25')
-
-    # Add routing for reaching networks that aren't directly connected
-    # H1 to H2
-    info(r1.cmd("route add -net 10.0.1.0/24 gw 192.168.10.177"))
-    info(r2.cmd("route add -net 10.0.1.0/24 gw 192.168.12.176"))
-    # # H2 to H1
-    info(r4.cmd("ip route add to 10.0.0.0/24 via 192.168.13.177"))
-    info(r3.cmd("ip route add to 10.0.0.0/24 via 192.168.11.176"))
-
+    net = Mininet(topo=topo, autoStaticArp=True)
     net.start()
-    info('*** Routing Table on Router:\n')
+    h1, h2, r1, r2, r3, r4 = [net[x] for x in ['h1', 'h2', 'r1', 'r2', 'r3', 'r4']]
+
+    h1.cmd('ifconfig h1-eth0 192.168.10.1/24')
+    h1.cmd('route add default gw 192.168.10.2')
+    r1.cmd('ifconfig r1-eth0 192.168.10.2/24')
+
+    h2.cmd('ifconfig h2-eth0 192.168.20.1/24')
+    h2.cmd('route add default gw 192.168.20.2')
+    r4.cmd('ifconfig r4-eth0 192.168.20.2/24')
+
+    r1.cmd('ifconfig r1-eth1 192.168.12.1/24')
+    r2.cmd('ifconfig r2-eth0 192.168.12.2/24')
+
+    r1.cmd('ifconfig r1-eth2 192.168.13.1/24')
+    r3.cmd('ifconfig r3-eth0 192.168.13.3/24')
+    r3.cmd('route add default gw 192.168.13.1')
+
+    r2.cmd('ifconfig r2-eth1 192.168.24.2/24')
+    r2.cmd('route add default gw 192.168.24.4')
+    r4.cmd('ifconfig r4-eth1 192.168.24.4/24')
+
+    r3.cmd('ifconfig r3-eth1 192.168.34.3/24')
+    r4.cmd('ifconfig r4-eth2 192.168.34.4/24')
+    r4.cmd('route add default gw 192.168.34.3')
+
     for i in [1, 2, 3, 4]:
-        info(net[f'r{i}'].cmd('route'))
+        info(f'*** Routing Table on Router r{i}:\n')
+        info(net[f'r{i}'].cmd('route') + '\n')
 
     CLI(net)
     net.stop()
