4616abf34b9cbf984ab1a0ac28ea626d798514d4
evenyoung <yiyun@igola.com>
2022-01-11 01:24:35 -0500

    [hw3]MyTopo.py:fix everything

--- hw3/MyTopo.py
+++ hw3/MyTopo.py
@@ -6,7 +6,7 @@
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.node import Node
-from mininet.log import setLogLevel, info, debug
+from mininet.log import setLogLevel, info
 from mininet.cli import CLI
 
 
@@ -26,16 +26,16 @@ class LinuxRouter(Node):
 class NetworkTopo(Topo):
     def build(self, **_opts):
         # Add routers
-        r4 = self.addHost('r4', cls=LinuxRouter)
+        r1 = self.addHost('r1', cls=LinuxRouter)
         r2 = self.addHost('r2', cls=LinuxRouter)
         r3 = self.addHost('r3', cls=LinuxRouter)
-        r1 = self.addHost('r1', cls=LinuxRouter)
-        h2 = self.addHost('h2')
+        r4 = self.addHost('r4', cls=LinuxRouter)
         h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
 
         # Adding host-router connection
-        self.addLink(h2, r4, intfName1='h2-eth0', intfName2='r4-eth0')
         self.addLink(h1, r1, inftname1='h1-eth0', inftname2='r1-eth0')
+        self.addLink(h2, r4, intfName1='h2-eth0', intfName2='r4-eth0')
 
         # Add router-router connection
         self.addLink(r4, r2, intfName1='r4-eth1', intfName2='r2-eth0')
@@ -48,48 +48,54 @@ def run():
     topo = NetworkTopo()
     net = Mininet(topo=topo, autoStaticArp=True)
     net.start()
-    h2, h1, r4, r2, r3, r1 = [net[x] for x in ['h2', 'h1', 'r4', 'r2', 'r3', 'r1']]
+    h1, h2, r4, r2, r3, r1 = [net[x] for x in ['h1', 'h2', 'r4', 'r2', 'r3', 'r1']]
 
-    h2.cmd('ifconfig h2-eth0 192.168.10.1/24')
-    h2.cmd('route add default gw 192.168.10.2')
-    r4.cmd('ifconfig r4-eth0 192.168.10.2/24')
+    h1.cmd('ip address add 192.168.10.10/24 dev h1-eth0')
+    r1.cmd('ip address add 192.168.10.12/24 dev r1-eth0')
+    h1.cmd('ip route add default via 192.168.10.12 dev h1-eth0')
 
-    h1.cmd('ifconfig h1-eth0 192.168.20.1/24')
-    h1.cmd('route add default gw 192.168.20.2')
-    r1.cmd('ifconfig r1-eth0 192.168.20.2/24')
+    h2.cmd('ip address add 192.168.20.10/24 dev h2-eth0')
+    r4.cmd('ip address add 192.168.20.12/24 dev r4-eth0')
+    h2.cmd('ip route add default via 192.168.20.12 dev h2-eth0')
 
-    r4.cmd('ifconfig r4-eth1 192.168.12.1/24')
-    r2.cmd('ifconfig r2-eth0 192.168.12.2/24')
+    r1.cmd('ip address add 192.168.12.12/24 dev r1-eth1')
+    r2.cmd('ip address add 192.168.12.21/24 dev r2-eth1')
+    r1.cmd('ip route add default via 192.168.12.21 dev r1-eth1')
+    r2.cmd('route add -net 192.168.10.0/24 gw 192.168.12.12')
 
-    r4.cmd('ifconfig r4-eth2 192.168.13.1/24')
-    r3.cmd('ifconfig r3-eth0 192.168.13.3/24')
-    r3.cmd('route add default gw 192.168.13.1')
+    r1.cmd('ip address add 192.168.13.13/24 dev r1-eth2')
+    r3.cmd('ip address add 192.168.13.31/24 dev r3-eth1')
+    r3.cmd('ip route add default via 192.168.13.13 dev r3-eth1')
 
-    r2.cmd('ifconfig r2-eth1 192.168.24.2/24')
-    r2.cmd('route add default gw 192.168.24.4')
-    r1.cmd('ifconfig r1-eth1 192.168.24.4/24')
+    r2.cmd('ip address add 192.168.24.24/24 dev r2-eth0')
+    r4.cmd('ip address add 192.168.24.42/24 dev r4-eth1')
+    r2.cmd('ip route add default via 192.168.24.42 dev r2-eth0')
 
-    r3.cmd('ifconfig r3-eth1 192.168.34.3/24')
-    r1.cmd('ifconfig r1-eth2 192.168.34.4/24')
-    r1.cmd('route add default gw 192.168.34.3')
-
-    # Add routes
-    r4.cmd("ip route add to 192.168.20.0/24 via 192.168.12.2")
-    r2.cmd("ip route add to 192.168.20.0/24 via 192.168.24.4")
-    r1.cmd("ip route add to 192.168.10.0/24 via 192.168.34.3")
-    r3.cmd("ip route add to 192.168.10.0/24 via 192.168.13.1")
+    r3.cmd('ip address add 192.168.34.34/24 dev r3-eth0')
+    r4.cmd('ip address add 192.168.34.43/24 dev r4-eth2')
+    r4.cmd('ip route add default via 192.168.34.34 dev r4-eth2')
+    r3.cmd('route add -net 192.168.20.0/24 gw 192.168.34.43')
 
     for i in ['h1', 'h2', 'r1', 'r2', 'r3', 'r4']:
+        # delete default IP that mininet assigned to the interface xx-eth0
+        for ip in net[f'{i}'].cmd("hostname -I").split(" "):
+            if ip.startswith("10.0.0"):
+                net[f'{i}'].cmd(f"ip addr del {ip}/8 dev {i}-eth0")
+                break
+        # print ip config
         info(f'*** IP config on node {i}:\n')
-        info(net[f'{i}'].cmd('ifconfig') + '\n')
+        info(net[f'{i}'].cmd('ip a') + '\n')
 
     for i in ['r1', 'r2', 'r3', 'r4']:
         info(f'*** Routing Table on Router {i}:\n')
-        info(net[f'{i}'].cmd('netstat -nr') + '\n')
+        # info(net[f'{i}'].cmd('netstat -nr') + '\n')
+        info(net[f'{i}'].cmd('route -n') + '\n')
 
-    # print traceroute output(pre-install `traceroute` on Ubuntu)
+    # print traceroute output
     info("*** trace route output from h1 to h2\n")
-    info(h1.cmd("traceroute -I 192.168.10.1") + "\n")
+    info(h1.cmd("traceroute -I 192.168.20.10") + "\n")
+    info("*** trace route output from h2 to h1\n")
+    info(h2.cmd("traceroute -I 192.168.10.10") + "\n")
 
     CLI(net)
     net.stop()
